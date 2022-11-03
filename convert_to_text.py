16af67f8e7b06a06422ede6586eef6c5e1b293bb
minturtle <minseok0425@kakao.com>
2022-11-03 23:27:25 +0900

    모듈화

--- convert_to_text.py
+++ convert_to_text.py
@@ -4,35 +4,53 @@ from tokenizers import SentencePieceBPETokenizer
 from transformers import BartForConditionalGeneration
 import torch
 
-#whisper로 음성 파일 텍스트 추출
-whisper_model = whisper.load_model("base")
+class Converter:
+    __whisper_model = None
+    __model = None
+    __tokenizer = None
 
-text_original = whisper_model.transcribe("audio-ko.mp3")['text']
-print("원문 : " + text_original)
-
-
-#kobart로 텍스트 파일 요약
-def tokenizer():
-    tokenizer = PreTrainedTokenizerFast.from_pretrained('gogamza/kobart-summarization')
-    return tokenizer
+    def __init__(self):
+        self.__whisper_model = whisper.load_model("base")
+        self.__tokenizer = self.__get_tokenizer()
+        self.__model = self.__get_model()
 
 
-def get_model():
-    model = BartForConditionalGeneration.from_pretrained('gogamza/kobart-summarization')
-    model.eval()
-    return model
+    # whisper로 음성 파일 텍스트 추출
+    def convert(self, audio_file):
+        text_original = self.__whisper_model.transcribe("audio-ko.mp3")['text']
+        return text_original
+
+    #kobart로 텍스트 파일 요약
+    def get_summary(self, text_original):
+        model = self.__model
+        tokenizer = self.__tokenizer
+
+        raw_input_ids = tokenizer.encode(text_original)
+        input_ids = [tokenizer.bos_token_id] + \
+                    raw_input_ids + [tokenizer.eos_token_id]
+
+        summary_ids = model.generate(torch.tensor([input_ids]),
+                                     max_length=256,
+                                     early_stopping=True,
+                                     repetition_penalty=2.0)
+        summary = tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
+        print("요약 : " + summary)
+
+    def __get_tokenizer(self):
+        tokenizer = PreTrainedTokenizerFast.from_pretrained('gogamza/kobart-summarization')
+        return tokenizer
 
 
-model = get_model()
-tokenizer = tokenizer()
+    def __get_model(self):
+        model = BartForConditionalGeneration.from_pretrained('gogamza/kobart-summarization')
+        model.eval()
+        return model
 
-raw_input_ids = tokenizer.encode(text_original)
-input_ids = [tokenizer.bos_token_id] + \
-            raw_input_ids + [tokenizer.eos_token_id]
 
-summary_ids = model.generate(torch.tensor([input_ids]),
-                     max_length=256,
-                     early_stopping=True,
-                     repetition_penalty=2.0)
-summary = tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
-print("요약 : " + summary)
+
+converter = Converter()
+
+original = converter.convert("audio-ko.mp3")
+summary = converter.get_summary(original)
+
+print(original, summary)
