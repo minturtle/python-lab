97a5149884c32d82e35c4f25c71cc84db61ceaa5
minturtle <minseok0425@kakao.com>
2022-11-13 14:15:17 +0900

    파일 이름  uuid 로 저장

--- pythonserver/convert/views.py
+++ pythonserver/convert/views.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from modules.audio_converter import Converter
+import uuid
+from os import path
 
 import json
 
@@ -14,10 +18,14 @@ def index(request):
 def fileUpload(request):
     try:
         if request.method == 'POST':
+            file = request.FILES.get("file")
+            if(file == None):
+                return HttpResponse(json.dumps({"msg" : "no file uploaded"}).encode('utf8'), content_type="application/json", status=400)
 
-            file = request.FILES["file"]
+            name,extension = path.splitext(file.name)
+
             fs = FileSystemStorage(location='uploads/audio', base_url='uploads/audio')
-            filename = fs.save(file.name, file)
+            filename = fs.save(str(uuid.uuid4()) + extension, file)
             convert_result = converter.convert(fs.url(filename))
 
             text_original = convert_result['text']
@@ -25,6 +33,6 @@ def fileUpload(request):
 
             text_summary = converter.get_summary(text_original) if language == 'ko' else '한글만 요약이 가능합니다.'
             return HttpResponse(json.dumps({"original_text" : text_original, "summary_text" : text_summary}).encode('utf8'), content_type="application/json")
-        return HttpResponse(json.dumps({"msg" : "invalid request"}).encode('utf8'), content_type="application/json", status=400)
+        return HttpResponse(json.dumps({"msg" : "invalid request method"}).encode('utf8'), content_type="application/json", status=400)
     except Exception as e:
         return HttpResponse(json.dumps({"msg" : e}), content_type="application/json", status=400)
